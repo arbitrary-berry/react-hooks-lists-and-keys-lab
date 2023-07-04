51be0c21fd4b5b7d0cb7267ccf4efb252d8e41b0
arbitrary-berry <alyssa.brooksong@gmail.com>
2023-07-03 17:09:27 -0700

    completed assignment

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,14 +1,48 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
+
+
 function ProjectList({ projects }) {
   console.log(projects);
+  const ProjectItems = projects.map((project) => (
+    <ProjectItem
+      key={project.id}
+      name={project.name}
+      about={project.about}
+      technologies={project.technologies}
+      />
+  ))
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{ProjectItems}</div>
     </div>
   );
 }
 
 export default ProjectList;
+
+// projects: [
+//   {
+//     id: 1,
+//     name: "Reciplease",
+//     about: "A recipe tracking app",
+//     technologies: ["Rails", "Bootstrap CSS"],
+//   },
+//   {
+//     id: 2,
+//     name: "Kibbles N Bitz",
+//     about: "Tinder for dogs",
+//     technologies: ["React", "Redux"],
+//   },
+//   {
+//     id: 3,
+//     name: "Alienwares",
+//     about: "Etsy for aliens",
+//     technologies: ["React", "Redux", "Rails"],
+//   },
+// ],
+// };
+
+// export default user;
